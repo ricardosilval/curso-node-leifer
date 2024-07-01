0c4eeb852c8aba4c739c3366d01c2538798ec6b7
ricardosilval <rsilvaldeg@gmail.com>
2024-07-01 13:41:59 -0400

    rutea

---
+++ routes/tracks.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router();
+
+//TODO http://localhost/tracks GET,POST,DELETE, PUT
+
+router.get("/", (req,res)=>{
+    const data = ["hola", "mundo"]
+    
+    res.send(data)
+})
+
+
+module.exports = router
